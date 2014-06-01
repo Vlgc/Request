192322e7994db0ade24f1f991413d64ea5113cf5
Vlgc <vlgc@system-lords.de>
2014-06-01 09:47:21 +0200

    corrected line endings and converted tabs to spaces

--- Request.php
+++ Request.php
@@ -12,49 +12,49 @@ namespace Vlgc;
 
 class Request
 {
-	/**
-	 * Get $value for $key 
-	 * 
-	 * @param mixed $key
-	 * @param mixed $defaultReturn [default: null]
-	 * @return mixed $value
-	 */
-	public function get($key, $defaultReturn = null)
-	{
-		if (array_key_exists($key, $_REQUEST)) {
-			return $_REQUEST[$key];
-		}
-		
-		return $defaultReturn;
-	}
-	
-	/**
-	 * Get $params 
-	 * 
-	 * @return array $params
-	 */
-	public function getParams()
-	{
-		if (is_array($_REQUEST)) {
-			return $_REQUEST;
-		}
-		
-		return array();
-	}
-	
-	/**
-	 * Get $value for $key 
-	 * 
-	 * @param mixed $key
-	 * @param mixed $defaultReturn [default: null] 
-	 * @return mixed $value
-	 */
-	public function getServer($key, $defaultReturn = null)
-	{
-		if (array_key_exists($key, $_SERVER)) {
-			return $_SERVER[$key];
-		}
-		
-		return $defaultReturn;
-	}
+    /**
+     * Get $value for $key
+     *
+     * @param mixed $key
+     * @param mixed $defaultReturn [default: null]
+     * @return mixed $value
+     */
+    public function get($key, $defaultReturn = null)
+    {
+        if (array_key_exists($key, $_REQUEST)) {
+            return $_REQUEST[$key];
+        }
+
+        return $defaultReturn;
+    }
+
+    /**
+     * Get $params
+     *
+     * @return array $params
+     */
+    public function getParams()
+    {
+        if (is_array($_REQUEST)) {
+            return $_REQUEST;
+        }
+
+        return array();
+    }
+
+    /**
+     * Get $value for $key
+     *
+     * @param mixed $key
+     * @param mixed $defaultReturn [default: null]
+     * @return mixed $value
+     */
+    public function getServer($key, $defaultReturn = null)
+    {
+        if (array_key_exists($key, $_SERVER)) {
+            return $_SERVER[$key];
+        }
+
+        return $defaultReturn;
+    }
 }
